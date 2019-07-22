ae417cfeccd84b22c1ef41e3a98ec6ce4bc0ee93
Ahmet Hüseyin DOK <off.ahmethuseyindok@gmail.com>
2019-07-22 14:03:17 +0300

    updated pm2

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ echo 'cloning project from github'
 ssh ubuntu@$machine -i ~/Documents/script/ahmet-huseyin.pem "git clone https://github.com/AhmetHuseyinDOK/express-hello.git"
 echo 'files cloned'
 echo 'getting dependencies'
-ssh ubuntu@$machine -i ~/Documents/script/ahmet-huseyin.pem "cd express-hello && npm install"
+ssh ubuntu@$machine -i ~/Documents/script/ahmet-huseyin.pem "cd express-hello && npm install && sudo npm install pm2 -g "
 echo 'got dependecies'
-ssh ubuntu@$machine -i ~/Documents/script/ahmet-huseyin.pem "cd express-hello && sudo killall node && sudo PORT=80 npm start-bg"
+ssh ubuntu@$machine -i ~/Documents/script/ahmet-huseyin.pem "cd express-hello &&  sudo PORT=80 npm run start-bg"
