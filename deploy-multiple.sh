c4c774cfcd6c36003805309fbcf7317cb73ff804
Ahmet Hüseyin DOK <off.ahmethuseyindok@gmail.com>
2019-07-22 13:41:22 +0300

    updated script

--- deploy-multiple.sh
+++ deploy-multiple.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+set -ex
+
+
 filename='servers'
 filelines=`cat $filename`
 echo "DON'T FORGET TO COMMIT & PUSH YOUR CHANGES"
 for server in $filelines ; do
-    sh ./deploy $server
+    sh ./deploy.sh $server
 done
