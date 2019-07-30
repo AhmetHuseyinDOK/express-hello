775f310fe2a4e9525eafed63dceac2e67da207bb
Ahmet Hüseyin DOK <off.ahmethuseyindok@gmail.com>
2019-07-30 11:39:49 +0300

    updated hello world

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 import axios from 'axios';
-app.get('/', async (req, res) => {
-    let {data} = await axios.get("http://169.254.169.254/latest/meta-data/public-hostname")
-    res.send(data);
+app.get('/', /** async */  (req, res) => {
+    // let {data} = await axios.get("http://169.254.169.254/latest/meta-data/public-hostname")
+    res.send("hello world");
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
